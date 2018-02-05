144a6e45412078fe57522f8669ecfe5cb51a4e70
wz2cool <wz2cool@live.cn>
2018-02-05 10:31:21 +0800

    add

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,7 @@
 import Document, { Head, Main, NextScript } from 'next/document'
-import flush from 'styled-jsx/server'
 
 export default class MyDocument extends Document {
-    static getInitialProps({ renderPage }) {
-        const { html, head, errorHtml, chunks } = renderPage()
-        const styles = flush()
-        return { html, head, errorHtml, chunks, styles }
-    }
-
-    render() {
+    public render() {
         return (
             <html>
                 <Head>
